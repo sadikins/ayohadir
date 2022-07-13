6fd0f0db918ae9ee8800e526aed8a2d50d84a682
sadikins <msadikin1989@gmail.com>
2022-07-13 18:27:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+  const [employees, setEmployees] = useState([])
+
+  // async
+  const getEmployees = async () => {
+    const {data} = await axios("https://ayohadir.id/v1/absen")
+    setEmployees(data.results)
+  }
+
+  useEffect(() => {
+
+    getEmployees()
+
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <h2>AYOHADIR</h2>
+
+      <table>
+        <thead>
+          <tr>
+            <th>User ID</th>
+            <th>Nama Karyawan</th>
+            <th>Jam Absen</th>
+          </tr>
+        </thead>
+        <tbody>
+       
+
+
+    {employees.map((result, index)=>{
+
+      return (
+
+         
+
+              <tr key={index}>
+                <td>{result.user_id}</td>
+                <td>{result.karyawan}</td>
+                <td>{result.jam_absen}</td>
+              </tr>
+          
+           
+      )
+
+    })}
+     </tbody>
+
+          </table>
+
+
+        </div>
+
+     
+   
   );
 }
 
